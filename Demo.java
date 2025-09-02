eeb5047b89e0f6de440a4aac3f076de54e9c6a62
239x1a2908 <239x1a2908@gprec.ac.in>
2025-09-02 16:35:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Demo {
+   public static void main(String[] args) {
+	   System.out.println(10);
+	   System.out.println(10+20);
+	   System.out.println(10+20+"Value is ");
+	   System.out.println(10+20+"Value is "+10);
+	   System.out.println(10+20+"Value is "+10+20);
+	   System.out.println(10+20+"Value is"+(10+20));
+   }
+}
