58b296515696733005f9d7003fe15563bd00f2a5
239x1a2908 <239x1a2908@gprec.ac.in>
2025-09-02 17:11:30 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Person {
+	String Name;
+	int Age;
+	Static String Nationality = "Indian";
+	public static void main(String[] args) {
+		System.out.println(Nationality);
+		System.out.println(Person Nationality);
+		System.out.println(Name);
+	}
