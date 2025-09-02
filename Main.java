0a42ffc247a5f5015aa10aa8aeae397b2b5e026c
239x1a2908 <239x1a2908@gprec.ac.in>
2025-09-02 17:11:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Main {
+	public static void main (String[] args) {
+		System.out.print("Java");
+		System.out.print("SQL");
+		System.out.print("Python");
+		System.out.println("Java");
+		System.out.println("SQL");
+		System.out.println("Python");
+	}
+}
