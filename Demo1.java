f50da0c2912ecb1f8c425ad762af519e79f4b9c8
239x1a2908 <239x1a2908@gprec.ac.in>
2025-09-02 16:35:39 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		int i=99;
+		double d=124.3;
+		System.out.println("i="+i);
+		System.out.println("d="+d);
+		System.out.println("After primative type conversion")
+		d=i;
+		System.out.println("i="+i);
+		System.out.println("d="+d);
+	}
+}
