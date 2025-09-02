b7e578ef0aef5a80ad7b3d45792531a22e265def
239x1a2908 <239x1a2908@gprec.ac.in>
2025-09-02 17:11:51 +0530

    Add files via upload

---
+++ Student3.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Student3 {
+	String StdName;
+	int Stdid;
+	String CollegeName;
+	public static void main(String[]args) {
+		Student3 s1=new Student3();
+		System.out.println(s1);
+	}
+}
