6df952143c25ed8885fec5248709d6832bcf207f
239x1a2908 <239x1a2908@gprec.ac.in>
2025-09-02 16:35:19 +0530

    Add files via upload

---
+++ Student2.java
@@ -0,0 +1,46 @@
+package day1;
+
+public class Student2 {
+	String StdName;
+	int RegNum;
+	String Branch;
+	double Percentage;
+	public static void main(String[] args) {
+		Student2 S1=new Student2();
+		S1.StdName="cherry";
+		S1.RegNum=1001;
+		S1.Branch="CSE";
+		S1.Percentage=89.9;
+		System.out.println("Student Name :"+S1.StdName);
+		System.out.println("Student Register Number:"+S1.RegNum);
+		System.out.println("Student Branch :"+S1.Branch);
+		System.out.println("Student Percentage:"+S1.Percentage);
+		Student2 S2=new Student2();
+		S2.StdName="Ashritha";
+		S2.RegNum=1002;
+		S2.Branch="CSE";
+		S2.Percentage=89.0;
+		System.out.println("Student Name :"+S2.StdName);
+		System.out.println("Student Register Number:"+S2.RegNum);
+		System.out.println("Student Branch :"+S2.Branch);
+		System.out.println("Student Percentage:"+S2.Percentage);
+		Student2 S3=new Student2();
+		S3.StdName="Keerthna";
+		S3.RegNum=1003;
+		S3.Branch="CSE";
+		S3.Percentage=85.9;
+		System.out.println("Student Name :"+S3.StdName);
+		System.out.println("Student Register Number:"+S3.RegNum);
+		System.out.println("Student Branch :"+S3.Branch);
+		System.out.println("Student Percentage:"+S3.Percentage);
+		Student2 S4=new Student2();
+		S4.StdName="Udaya";
+		S4.RegNum=1004;
+		S4.Branch="CSE";
+		S4.Percentage=70.8;
+		System.out.println("Student Name :"+S1.StdName);
+		System.out.println("Student Register Number:"+S1.RegNum);
+		System.out.println("Student Branch :"+S1.Branch);
+		System.out.println("Student Percentage:"+S1.Percentage);
+	}
+}
